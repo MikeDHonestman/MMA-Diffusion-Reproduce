dc590a6d12d8686a1199909abac8972a0730ca57
MikeDHonestman <m18514034690@163.com>
2026-04-13 16:56:15 +0800

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-from google import genai
-
-# The client gets the API key from the environment variable `GEMINI_API_KEY`.
-client = genai.Client(api_key="AIzaSyCmIuYGM6LVnztvfJ48PPIHF1eTakUe5JY")
-
-response = client.models.generate_content(
-    model="gemini-3-flash-preview", contents="hi"
-)
-print(response.text) 
